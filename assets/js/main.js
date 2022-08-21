dd49f3aee760227f3f64aebb3343f79e6bcb8ed8
Ebalin1411 <ebalinrosecse2006@gmail.com>
2022-08-21 19:05:32 +0530

    feat: Dom Manupulation Applied

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,49 @@
 function initalize(){
     //Hiding input and output Containers
     const  outputContainerElement= document.querySelector('.output');
-    if( outputContainerElement ==null)
-    {
+    if( outputContainerElement ==null){
        console.log('element not found')
     }
-    else
-    {
-        //document.querySelector('.output').style.display='none';
-        outputContainerElement.style.visibility="hidden";
-        document.querySelector('.input').style.visibility="hidden";
-     }
+    else{
+        outputContainerElement.style.visibility='hidden';
+        document.querySelector('.input').style.visibility='hidden';
+        }
+    }    
     
-}
-
 function onKeyPress(event){
-
     console.log('Key pressed', event.key);
-
     if (event.key === 'Enter'){
         const inputElement = document.querySelector('.input input');
-        const userResponse = inputElement.value;
-        
+        const userResponse = inputElement.value;        
         console.log('User Entered', userResponse);
+        if(userResponse ==='hot'){
+            prepareHotCoffee();
+        } 
+        else if(userResponse === 'cold'){
+            prepareColdCoffee();
+        } 
+        else if(userResponse === 'instant'){
+            prepareInstantCoffee();
+        } 
+        else{
+            console.log('Please Enter hot or cold')    
         }
+        
+    }
 
 }
 
 function onButtonClick(){
     const  outputContainerElement= document.querySelector('.output');
-    if( outputContainerElement ==null)
-    {
+
+    if( outputContainerElement ==null){
        console.log('element not found')
     }
-    else
-    {
-        //document.querySelector('.output').style.display='none';
-        outputContainerElement.style.visibility="visible";
-        document.querySelector('.input').style.visibility="visible";        
+    else{        
+        outputContainerElement.style.visibility='visible';
+        document.querySelector('.input').style.visibility='visible';        
     }
     //create an element for asking questions
-
     const paraElement =  document.createElement('p');
     paraElement.textContent="Do you like to have a hot or cold coffee";
     paraElement.style.color='grey'
@@ -50,26 +53,37 @@ function onButtonClick(){
     const inputElement= document.querySelector('.input input');
     inputElement.addEventListener('keydown',onKeyPress)
 
-    
-    //alert('Hey I am ready to take to Orders');
-    //const userResponse =prompt('Do you want hot or cold coffee');
-    //console.log('userResponse::',userResponse);
-    //if(userResponse==="hot"){
-         
-    //    prepareHotCoffee();
-    //}
-    //else {
-    //  alert('Going to prepare Cold coffee');
- 
-    //    }
   }
 
   function prepareHotCoffee(){
-    alert('Going to prepare hot coffee');
-    const userResponse=prompt ('Do you want instant or filter coffee?');
-    if (userResponse==="instant"){
-        alert('Going to prepare instant coffee');
+    
+    document.querySelector('.output span').textContent ="Going to prepare hot coffee";
+    const paraElement = document.querySelector('.output p');
+    paraElement.textContent = 'Do you want instant or filter coffee?';
+
+    const inputElement = document.querySelector('.input input');
+    inputElement.value =''
+    
+    
+    const userResponse = inputElement.value; 
+
+    if (userResponse==="instant"){        
+        prepareInstantCoffee();
         }else{
-        alert('Going to prepare filter coffee');
+        prepareFilterCoffee();
         }
   }
+  
+  function  prepareInstantCoffee(){
+    document.querySelector('.output span').textContent ="Going to prepare Instant coffee";
+    document.querySelector('.output p').style.visibility ='hidden';
+    document.querySelector('.input').style.visibility='hidden';
+  }
+  
+  function prepareFilterCoffee(){
+
+  }
+
+  function prepareColdCoffee(){
+
+}
